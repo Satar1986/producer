7d9be959a759f30a7fac8a1255fa742ece6b7912
Oleg <batinoleg348@gmail.com>
2025-09-16 20:50:25 +0300

    Commit producer (+update) spring boot 3.3.3 swagger ui Debug

--- src/main/java/org/example/config/TopicConfig.java
+++ src/main/java/org/example/config/TopicConfig.java
@@ -1,12 +1,51 @@
 package org.example.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.example.event.ProductEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class TopicConfig {
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+
+    @Value("${spring.kafka.producer.properties.enable.idempotence}")
+    private String enableIdempotence;
+
+
+
+    Map<String, Object> producerConfigs() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, enableIdempotence);
+        return config;
+    }
+    @Bean
+    ProducerFactory<String, ProductEvent> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+    @Bean
+    KafkaTemplate<String, ProductEvent> kafkaTemplate() {
+        return new KafkaTemplate<String, ProductEvent>(producerFactory());
+    }
     @Bean
   public   NewTopic createTopic() {
         return TopicBuilder.name("Account_bank")
